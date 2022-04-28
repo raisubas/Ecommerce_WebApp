0cd20f2d38da6ca6b67590d6debb216f67b8149d
raisubas <raisubas234@gmail.com>
2022-04-28 17:55:56 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function removeCookie(){
+    document.querySelector("#cookiebar").remove()
+}
+function changeImage(element){
+    element.setAttribute('src', 'images/succulents-2.jpg')
+
+}
+function changeBack(element){
+    element.setAttribute('src', 'images/succulents-1.jpg')
+}
